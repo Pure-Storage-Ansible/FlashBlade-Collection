460a55c126894bfb8850b75034a45876c81eb4fb
Simon Dodsley <simon@purestorage.com>
2020-04-03 15:20:26 -0400

    Add SDK check

--- collections/ansible_collections/purestorage/flashblade/plugins/module_utils/purefb.py
+++ collections/ansible_collections/purestorage/flashblade/plugins/module_utils/purefb.py
@@ -56,29 +56,32 @@ def get_blade(module):
     blade_name = module.params['fb_url']
     api = module.params['api_token']
 
-    if blade_name and api:
-        blade = PurityFb(blade_name)
-        blade.disable_verify_ssl()
-        try:
-            blade.login(api)
-            versions = blade.api_version.list_versions().versions
-            if API_AGENT_VERSION in versions:
-                blade._api_client.user_agent = user_agent
-        except Exception:
-            module.fail_json(msg="Pure Storage FlashBlade authentication failed. Check your credentials")
-    elif environ.get('PUREFB_URL') and environ.get('PUREFB_API'):
-        blade = PurityFb(environ.get('PUREFB_URL'))
-        blade.disable_verify_ssl()
-        try:
-            blade.login(environ.get('PUREFB_API'))
-            versions = blade.api_version.list_versions().versions
-            if API_AGENT_VERSION in versions:
-                blade._api_client.user_agent = user_agent
-        except Exception:
-            module.fail_json(msg="Pure Storage FlashBlade authentication failed. Check your credentials")
+    if HAS_PURITY_FB:
+        if blade_name and api:
+            blade = PurityFb(blade_name)
+            blade.disable_verify_ssl()
+            try:
+                blade.login(api)
+                versions = blade.api_version.list_versions().versions
+                if API_AGENT_VERSION in versions:
+                    blade._api_client.user_agent = user_agent
+            except Exception:
+                module.fail_json(msg="Pure Storage FlashBlade authentication failed. Check your credentials")
+        elif environ.get('PUREFB_URL') and environ.get('PUREFB_API'):
+            blade = PurityFb(environ.get('PUREFB_URL'))
+            blade.disable_verify_ssl()
+            try:
+                blade.login(environ.get('PUREFB_API'))
+                versions = blade.api_version.list_versions().versions
+                if API_AGENT_VERSION in versions:
+                    blade._api_client.user_agent = user_agent
+            except Exception:
+                module.fail_json(msg="Pure Storage FlashBlade authentication failed. Check your credentials")
+        else:
+            module.fail_json(msg="You must set PUREFB_URL and PUREFB_API environment variables "
+                                 "or the fb_url and api_token module arguments")
     else:
-        module.fail_json(msg="You must set PUREFB_URL and PUREFB_API environment variables "
-                             "or the fb_url and api_token module arguments")
+        module.fail_json(msg="purity_fb SDK not installed.")
     return blade
 
 
