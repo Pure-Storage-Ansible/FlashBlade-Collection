6f98c99d866e74c7e5d210a580b75a4f55401b73
mahendra-maplelabs <mahendra.jakhar@maplelabs.com>
2020-08-14 15:46:41 +0530

    Fix sanity test issue

--- collections/ansible_collections/purestorage/flashblade/plugins/modules/purefb_info.py
+++ collections/ansible_collections/purestorage/flashblade/plugins/modules/purefb_info.py
@@ -214,8 +214,6 @@ purefb_info:
                 "destroyed": false,
                 "fast_remove": false,
                 "hard_limit": true,
-                "policies": ["weekly", "daily"]
-                ],
                 "nfs_rules": "10.21.255.0/24(rw,no_root_squash)",
                 "provisioned": 21474836480,
                 "snapshot_enabled": false
@@ -224,7 +222,6 @@ purefb_info:
                 "destroyed": false,
                 "fast_remove": false,
                 "hard_limit": false,
-                "policies": [],
                 "provisioned": 1073741824,
                 "snapshot_enabled": false
             }
