5b7085adc35b222793753c7ee772af90c39343df
Simon Dodsley <simon@purestorage.com>
2021-04-20 12:21:24 -0400

    Update purefb_info.py

--- collections/ansible_collections/purestorage/flashblade/plugins/modules/purefb_info.py
+++ collections/ansible_collections/purestorage/flashblade/plugins/modules/purefb_info.py
@@ -929,16 +929,18 @@ def generate_kerb_dict(blade):
 
 def generate_ad_dict(blade):
     ad_info = {}
-    ad_account = list(blade.get_active_directory().items)[0]
-    ad_info[ad_account.name] = {
-        "computer": ad_account.computer_name,
-        "domain": ad_account.domain,
-        "directory_servers": ad_account.directory_servers,
-        "kerberos_servers": ad_account.kerberos_servers,
-        "service_principals": ad_account.service_principal_names,
-        "join_ou": ad_account.join_ou,
-        "encryption_types": ad_account.encryption_types,
-    }
+    actve_directory = blade.get_active_directory()
+    if active_directory.total_item_count != 0:
+        ad_account = list(active_directory.items)[0]
+        ad_info[ad_account.name] = {
+            "computer": ad_account.computer_name,
+            "domain": ad_account.domain,
+            "directory_servers": ad_account.directory_servers,
+            "kerberos_servers": ad_account.kerberos_servers,
+            "service_principals": ad_account.service_principal_names,
+            "join_ou": ad_account.join_ou,
+            "encryption_types": ad_account.encryption_types,
+        }
     return ad_info
 
 
