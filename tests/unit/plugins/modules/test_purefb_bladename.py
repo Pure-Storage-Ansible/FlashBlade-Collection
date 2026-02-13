7427c4fbc3a13816150aab676834d2a5018d9d68
Simon Dodsley <simon@purestorage.com>
2026-02-13 15:27:07 -0500

    Add copyright header and docstring to test_purefb_bladename

--- tests/unit/plugins/modules/test_purefb_bladename.py
+++ tests/unit/plugins/modules/test_purefb_bladename.py
@@ -1,3 +1,8 @@
+# Copyright: (c) 2026, Pure Storage Ansible Team <pure-ansible-team@purestorage.com>
+# GNU General Public License v3.0+ (see COPYING.GPLv3 or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""Unit tests for purefb_bladename module."""
+
 from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
