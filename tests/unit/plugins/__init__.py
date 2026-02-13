a2e45a7178947f7dac21570dd918d780c0c2d0dd
Simon Dodsley <simon@purestorage.com>
2026-02-13 13:08:37 -0500

    Fix Ansible CI errors - make __init__.py files completely empty

--- tests/unit/plugins/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright: (c) 2026, Pure Storage Ansible Team <pure-ansible-team@purestorage.com>
-# GNU General Public License v3.0+ (see COPYING.GPLv3 or https://www.gnu.org/licenses/gpl-3.0.txt)
-
-"""Unit tests for plugins."""
-
-from __future__ import absolute_import, division, print_function
-
-__metaclass__ = type
